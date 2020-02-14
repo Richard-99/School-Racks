a9b0c19acc9b8a2adfbe55774b5037bddc50c8a5
Mayank Gupta <g.mayank82@yahoo.com>
2020-02-14 11:12:36 +0530

    Initial Commit

--- schoolracks/task/models.py
+++ schoolracks/task/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    student_id = models.CharField(max_length = 2)
+
+class Racks(models.Model):
+    racks_id = models.CharField(max_length = 2)
+    students = models.ManyToManyField('Student', related_name = 'racks', blank = True)
