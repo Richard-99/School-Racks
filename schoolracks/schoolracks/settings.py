e801858bec7be1ef3586a5ee9ca34798cdfd5366
Mayank Gupta <g.mayank82@yahoo.com>
2020-02-14 15:17:27 +0530

    Initial Commit

--- schoolracks/schoolracks/settings.py
+++ schoolracks/schoolracks/settings.py
@@ -42,15 +42,6 @@ INSTALLED_APPS = [
     'task'
 ]
 
-REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-    'rest_framework.permissions.IsAdminUser',
-    ],
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-    'rest_framework.authentication.TokenAuthentication',),
-    }
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
