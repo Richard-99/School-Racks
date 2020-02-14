70ec3506c976d655aeeaf9d47909c47b50ac7e0b
Mayank Gupta <g.mayank82@yahoo.com>
2020-02-14 14:38:47 +0530

    Initial Commit

--- schoolracks/task/serializer.py
+++ schoolracks/task/serializer.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 
 from django.contrib.auth.models import User
-from .models import Student, Racks
+from .models import Racks, Student
 
 class UserSerializer(serializers.ModelSerializer):
 
